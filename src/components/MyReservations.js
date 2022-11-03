4dfda0937097152b92e9b4fe0e8f81d53abd825b
Desor Guishny <desorguishny@gmail.com>
2022-11-03 14:46:42 -0400

    Fix my reservations title

--- src/components/MyReservations.js
+++ src/components/MyReservations.js
@@ -10,11 +10,8 @@ const MyReservations = () => {
     // const reservations = useSelector((state) => state.reservations.find((elem) => JSON.stringify(elem.user_id) === JSON.stringify(id)));
   return (    
     <div class="p-4 w-full bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <div class="flex justify-between items-center mb-4">
+        <div class="flex justify-center items-center mb-4">
             <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">My Reservations</h5>
-            <a href="/my_reservations" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
-                View all
-            </a>
       </div>
       <div class="flow-root">
             <ul class="divide-y divide-gray-200 dark:divide-gray-700">
